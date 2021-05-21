9b6f6cd18ea7317852f3415f8294c82cc88570f9
gelomacapuno <gelomacapuno@rocketmail.com>
2021-05-21 18:11:19 +0800

    Add Patient

--- src/GUI/AddForm.java
+++ src/GUI/AddForm.java
@@ -1,4 +1,27 @@
 package GUI;
 
-public class AddForm {
+import javax.swing.*;
+import java.awt.*;
+
+public class AddForm extends JFrame {
+
+    private JPanel AddRecordForm;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JFrame frame;
+
+    public AddForm(){
+        frame =new JFrame("Add Patient Form");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(1000,500));
+        frame.setResizable(false);
+
+        frame.add(AddRecordForm);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
