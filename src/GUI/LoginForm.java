5b00ddff148b55bc721ab660627cd8d9941a48d5
Alexander <alexanderjames.cebreiros@gmail.com>
2021-05-21 17:08:11 +0800

    Finished Basic Login Page

--- src/GUI/LoginForm.java
+++ src/GUI/LoginForm.java
@@ -2,6 +2,9 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class LoginForm extends JFrame{
     private JPasswordField DepCodeInput;
@@ -14,7 +17,7 @@ public class LoginForm extends JFrame{
     public LoginForm(){
         Frame =new JFrame("Login");
         Frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        Frame.setPreferredSize(new Dimension(250,200));
+        Frame.setPreferredSize(new Dimension(300,250));
         Frame.setResizable(false);
 
         Frame.add(panel1);
@@ -22,5 +25,17 @@ public class LoginForm extends JFrame{
         Frame.pack();
         Frame.setLocationRelativeTo(null);
         Frame.setVisible(true);
+        LoginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                char[] DeptCode = DepCodeInput.getPassword();
+                char[] correctDeptCode = new char[] {'1','2','3','4','5'};
+                if (Arrays.equals(DeptCode, correctDeptCode)){
+                    JOptionPane.showMessageDialog(null,"Hello World");
+                } else {
+                    JOptionPane.showMessageDialog(null,"Wrong Password");
+                }
+            }
+        });
     }
 }
