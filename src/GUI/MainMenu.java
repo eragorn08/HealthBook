55a3405784817870bcee4810aab30b38c6af1574
TheUn-fixed <53077791+TheUn-fixed@users.noreply.github.com>
2021-05-22 03:43:15 +0800

    Fixed label positions.

--- src/GUI/MainMenu.java
+++ src/GUI/MainMenu.java
@@ -16,18 +16,19 @@ public class MainMenu extends JFrame implements ActionListener{
         setNavPanel();
         setDisplayPanel();
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(640, 480);
-        setLayout(null);
-        setLocationRelativeTo(null);
-        setResizable(false);
         setVisible(true);
+        setSize(640, 480);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+//        setLayout(null);
+        setResizable(false);
 
     }
 
     public void setNavPanel(){
         NavPanel = new JPanel();
         NavPanel.setLayout(null);
+        NavPanel.setBackground(Color.DARK_GRAY);
 
         NavPanel.add(addButton = new JButton("Add"));
         NavPanel.add(searchButton = new JButton("Search"));
@@ -40,30 +41,29 @@ public class MainMenu extends JFrame implements ActionListener{
         addButton.addActionListener(this);
         searchButton.addActionListener(this);
 
-        NavPanel.setBounds(0, 0, 160, 480);
         addButton.setBounds(19, 56, 114, 44);
         searchButton.setBounds(19, 120, 114, 44);
 
+        NavPanel.setBounds(0, 0, 160, 480);
         NavPanel.setVisible(true);
-        NavPanel.setBackground(Color.DARK_GRAY);
         getContentPane().add(NavPanel);
     }
 
     public void setDisplayPanel(){
         DisplayPanel = new JPanel();
         DisplayPanel.setLayout(null);
+        DisplayPanel.setBackground(Color.cyan);
 
         DisplayPanel.add(greetingL = new JLabel("Welcome back, Good Afternoon!"));
         DisplayPanel.add(summaryL = new JLabel("Summary of Records"));
         greetingL.setFont(new Font("Sans Serif", Font.PLAIN, 21));
         summaryL.setFont(new Font("Sans Serif", Font.PLAIN, 18));
 
-        DisplayPanel.setBounds(160, 0, 480, 480);
-        greetingL.setBounds(25, 10, 480, 50);
-        summaryL.setBounds(150, 120, 200, 50);
+        greetingL.setBounds(180, 10, 480, 50);
+        summaryL.setBounds(310, 120, 200, 50);
 
+        DisplayPanel.setBounds(0, 0, 480, 480);
         DisplayPanel.setVisible(true);
-        DisplayPanel.setBackground(Color.cyan);
         getContentPane().add(DisplayPanel);
     }
 
