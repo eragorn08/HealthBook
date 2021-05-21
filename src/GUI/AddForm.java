a2b8d89f216f137cda7e5ce99d245c17ab4620c2
Alexander <alexanderjames.cebreiros@gmail.com>
2021-05-21 19:00:56 +0800

    Fixed Add Form

--- src/GUI/AddForm.java
+++ src/GUI/AddForm.java
@@ -10,12 +10,25 @@ public class AddForm extends JFrame {
     private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JRadioButton maleRadioButton;
+    private JRadioButton femaleRadioButton;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+    private JButton submitButton;
+    private JButton resetButton;
+    private JTextArea textArea1;
     private JFrame frame;
 
     public AddForm(){
         frame =new JFrame("Add Patient Form");
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(1000,500));
+        frame.setPreferredSize(new Dimension(1000,400));
         frame.setResizable(false);
 
         frame.add(AddRecordForm);
