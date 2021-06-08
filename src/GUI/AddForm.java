230db30b4017360d974f69514fa6706b43e80c36
gelomacapuno <gelomacapuno@rocketmail.com>
2021-06-09 00:44:54 +0800

    AddForm Started

--- src/GUI/AddForm.java
+++ src/GUI/AddForm.java
@@ -2,40 +2,33 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+//import javax.swing.JFrame;
 
-public class AddForm extends JFrame {
+public class AddForm extends JFrame implements ActionListener {
 
-    private JPanel AddRecordForm;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField textField6;
-    private JTextField textField7;
-    private JTextField textField8;
-    private JTextField textField9;
-    private JRadioButton maleRadioButton;
-    private JRadioButton femaleRadioButton;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
-    private JComboBox comboBox3;
-    private JButton submitButton;
-    private JButton resetButton;
-    private JTextArea textArea1;
-    private JFrame frame;
+    public AddForm() {
+        super("Add Form");
 
-    public AddForm(){
-        frame =new JFrame("Add Patient Form");
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(1000,400));
-        frame.setResizable(false);
+        setNavPanel();
 
-        frame.add(AddRecordForm);
-
-        frame.pack();
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
     }
 
+    public void setNavPanel(){
+        NavPanel = new JPanel();
+        NavPanel.setLayout(null);
+        NavPanel.setBackground(Color.DARK_GRAY);
+
+        NavPanel.add(add)
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == )
+    }
+
+    public JPanel NavPanel, DisplayPanel;
+    public JButton addButton, searchButton;
+    public JLabel greetingL, summaryL;
 }
