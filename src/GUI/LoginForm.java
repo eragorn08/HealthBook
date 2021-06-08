b95f8d7554bfeadc9f0e0a18e3ff23e48b11ac66
Alexander <alexanderjames.cebreiros@gmail.com>
2021-06-08 13:39:51 +0800

    Fixed Login and AddFormm

--- src/GUI/LoginForm.java
+++ src/GUI/LoginForm.java
@@ -4,38 +4,82 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Arrays;
 
-public class LoginForm extends JFrame{
-    private JPasswordField DepCodeInput;
-    private JLabel Label1;
-    private JPanel panel1;
-    private JButton LoginButton;
-    private JLabel Label2;
-    private JFrame Frame;
+public class LoginForm extends JFrame implements ActionListener{
 
-    public LoginForm(){
-        Frame =new JFrame("Login");
-        Frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        Frame.setPreferredSize(new Dimension(300,250));
-        Frame.setResizable(false);
+    public LoginForm() {
+        super("Health Book");
 
-        Frame.add(panel1);
+        SetDisplayPanel();
+        SetDisplayContainer();
 
-        Frame.pack();
-        Frame.setLocationRelativeTo(null);
-        Frame.setVisible(true);
-        LoginButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                char[] DeptCode = DepCodeInput.getPassword();
-                char[] correctDeptCode = new char[] {'1','2','3','4','5'};
-                if (Arrays.equals(DeptCode, correctDeptCode)){
-                    JOptionPane.showMessageDialog(null,"Hello World");
-                } else {
-                    JOptionPane.showMessageDialog(null,"Wrong Password");
-                }
-            }
-        });
+        setVisible(true);
+        setSize(1280, 720);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
     }
+
+    public void SetDisplayPanel(){
+        DisplayPanel = new JPanel();
+        DisplayPanel.setLayout(null);
+        DisplayPanel.setBackground(Color.getHSBColor(238, 130, 238));
+
+        //Login
+        DisplayPanel.add(DisplayTitle = new JLabel("LOG IN"));
+
+        DisplayTitle.setBounds(200,80,480,50);
+
+        DisplayTitle.setFont(new Font("Arial",Font.PLAIN,48));
+
+        //Text Fields
+        DisplayPanel.add(DeptCode = new JTextField("Dept Code"));
+
+        DeptCode.setBounds(150,200,250,40);
+
+        DisplayPanel.add(EmpID = new JTextField("Employee ID"));
+
+        EmpID.setBounds(150,260,250,40);
+
+        //Submit Button
+        DisplayPanel.add(Login = new JButton("Sign In"));
+
+        Login.setBounds(180,380,200,60);
+
+        Login.setFont(new Font("Arial",Font.PLAIN,40));
+
+        Login.addActionListener(this);
+
+        DisplayPanel.setBounds(380,50,560,580);
+        DisplayPanel.setVisible(true);
+        getContentPane().add(DisplayPanel);
+    }
+
+    public void SetDisplayContainer(){
+        DisplayContainer = new JPanel();
+        DisplayContainer.setLayout(null);
+        DisplayContainer.setBackground(Color.gray);
+
+        DisplayContainer.setBounds(0,0,320, 180);
+        getContentPane().add(DisplayContainer);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String Code = DeptCode.getText();
+        String CorrectCode = "Cardiology12345";
+        if (e.getSource() == Login) {
+            if (Code.equals(CorrectCode)) {
+                setVisible(false);
+                MainMenu mainMenu = new MainMenu();
+            } else {
+                JOptionPane.showMessageDialog(null, "Wrong Department Code");
+            }
+        }
+    }
+
+    public JPanel DisplayPanel, DisplayContainer;
+    public JTextField DeptCode, EmpID;
+    public JLabel DisplayTitle;
+    public JButton Login;
 }
